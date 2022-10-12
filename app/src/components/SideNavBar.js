617ddf23c18753ad1c5f554b40bdcc68f265b653
Alexon1999 <alexon1999@gmail.com>
2022-10-12 18:15:55 +0200

    fix SideNavBar component

--- app/src/components/SideNavBar.js
+++ app/src/components/SideNavBar.js
@@ -1,66 +1,173 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { Avatar, Stack} from '@mui/material';
+import { Avatar, IconButton} from '@mui/material';
 import { grey } from '@mui/material/colors';
 import HomeIcon from '@mui/icons-material/Home';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import MuiDrawer from '@mui/material/Drawer';
+import { styled, useTheme } from '@mui/material/styles';
 
 import daruma from '../assets/daruma.svg';
 
 const drawerWidth = 240;
 
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const DrawerFooter = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    position: 'relative',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
 function SideNavBar() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+
+  const toggleDrawer = () => {
+    setOpen(prev => !prev);
+  };
 
   return (
     <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
         variant="permanent"
-        anchor="left"
+        open={open}
       >
-        <Toolbar sx={{ bgcolor: grey[50] }}>
-          <Stack direction="row" spacing={2} sx={{display: 'flex', alignItems: 'center'}}>
-            <Avatar src={daruma} />
-            <Typography>Daruma Ball</Typography>
-          </Stack>
-        </Toolbar>
-        <Divider />
-        <List>
-          <ListItem disablePadding>
-              <ListItemButton onClick={() => navigate('/')}>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary='Accueil' />
-              </ListItemButton>
-            </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem disablePadding>
-              <ListItemButton onClick={() => navigate('/list')}>
-                <ListItemIcon>
-                  <FormatListBulletedIcon />
-                </ListItemIcon>
-                <ListItemText primary='Liste' />
-              </ListItemButton>
+      <List sx={{ bgcolor: grey[50] }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2,
+              }}
+              onClick={() => navigate('/')}>
+              <Avatar src={daruma} sx={{
+                  minWidth: 0,
+                  mr: open ? 1.8 : 'auto',
+                  justifyContent: 'center',
+                }} />
+              <ListItemText primary='Daruma Ball'
+                sx={{ opacity: open ? 1 : 0 }}
+                />
+            </ListItemButton>
           </ListItem>
-        </List>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+              onClick={() => navigate('/')}>
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary='Accueil'
+                sx={{ opacity: open ? 1 : 0 }}
+                />
+            </ListItemButton>
+          </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+              onClick={() => navigate('/list')}>
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}>
+                <FormatListBulletedIcon />
+              </ListItemIcon>
+              <ListItemText 
+                primary='Liste'
+                sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+        </ListItem>
+      </List>
+      <DrawerFooter 
+        sx={{left: open ? 'auto' : 0, right: open ? 0 : 'auto'}}
+        >
+        <IconButton onClick={toggleDrawer}>
+          {open ? <ChevronLeftIcon fontSize='large' /> : <ChevronRightIcon fontSize='large' />}
+        </IconButton>
+      </DrawerFooter>
     </Drawer>
   )
 }
