aa94152e8e6736d8a12e38232a6a29459c97d7af
Alexon1999 <alexon1999@gmail.com>
2022-10-12 18:16:42 +0200

    fix Completion tab on ListDarumaPage

--- app/src/ListDarumaPage.js
+++ app/src/ListDarumaPage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Tab, Tabs } from "@mui/material";
+import { Box, Tab, Tabs, tabsClasses } from "@mui/material";
 import DarumaCard from "./components/DarumaCard/DarumaCard";
 
 function ListDarumaPage() {
@@ -10,17 +10,28 @@ function ListDarumaPage() {
   };
 
   return (
-    <Box sx={{ margin: '10px 10px', width: '100%'}}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-        centered
-      >
-        <Tab label="Work in progress" />
-        <Tab label="Completed" />
-      </Tabs>
+    <Box sx={{ margin: '10px 10px', width: '100%', overflow: 'hidden'}}>
+      <Box sx={{
+        width: '100%', display: 'flex', justifyContent: 'center', overflowX: 'hidden'
+        }}>
+        <Tabs
+          sx={{
+            flexGrow: 1,
+            maxWidth: { xs: 320, sm: 480 },
+            [`& .${tabsClasses.scrollButtons}`]: {
+              '&.Mui-disabled': { opacity: 0.3 },
+            },
+          }}
+          value={value}
+          onChange={handleChange}
+          scrollButtons
+          aria-label="tabs indicating the balls evolution"
+          variant="scrollable"
+        >
+          <Tab label="Work in progress" />
+          <Tab label="Completed" />
+        </Tabs>
+      </Box>
       <Box sx={{ margin: '100px 0px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', columnGap: '50px', rowGap: '90px'}}>
         <DarumaCard />
         <DarumaCard />
